d0aa9784a5878e3faa5671349008e5c4dc16b014
Bear Sterns <bearsterns.dev@gmail.com>
2022-05-24 10:15:19 +0530

    exercise hours stats and bmi calculator
    Exercises 9.1-9.3

---
+++ bmiCalculator.ts
@@ -0,0 +1,67 @@
+interface PhysicalParams {
+  height: number;
+  weight: number;
+}
+
+
+const parseArguments = (args: Array<string>): PhysicalParams => {
+  if (args.length < 4) throw new Error("Too few arguments.");
+  if (args.length > 4) throw new Error("Too many arguments");
+
+  if (isNaN(Number(args[2]))) throw new Error("Enter a valid height");
+  if (isNaN(Number(args[3]))) throw new Error("Enter a valid weight");
+
+  return {
+    height: Number(args[2]),
+    weight: Number(args[3]),
+  };
+}
+
+
+const calculateBmi = (height: number, weight: number): string => {
+  if (weight == 0)  throw new Error("Weight cannot be zero");
+
+  const bmi: number = (height / (weight ** 2)) * 703;
+
+  if (bmi < 16.0) {
+    return "Underweight (Severe thinness)";
+  }
+  else if (bmi >= 16.0 && bmi <= 16.9) {
+    return "Underweight (Moderate thinness)";
+  }
+  else if (bmi >= 17.0 && bmi <= 18.4) {
+    return "Underweight (Mild thinness)";
+  }
+  else if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Normal (healthy weight)";
+  }
+  else if (bmi >= 25.0 && bmi <= 29.9) {
+    return "Overweight (Pre-obese)";
+  }
+  else if (bmi >= 30.0 && bmi <= 34.9) {
+    return "Obese (Class I)";
+  }
+  else if (bmi >= 35.0 && bmi <= 39.9) {
+    return "Obese (Class II	";
+  }
+  else {
+    return "Obese (Class III)";
+  }
+}
+
+
+try {
+  const {height, weight} = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+}
+catch(error: unknown) {
+  let errorMsg: string = "Something went wrong.";
+  if (error instanceof Error) {
+    errorMsg += " Error: " + error.message;
+  }
+  console.log(errorMsg);
+}
+
+
+// Silence block-scoped redeclaration error
+export {};
---
+++ exerciseCalculator.ts
@@ -0,0 +1,89 @@
+interface ExerciseResult {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+interface ExerciseStats {
+  exerciseHours: Array<number>;
+  target: number;
+}
+
+
+const parseArguments = (args: Array<string>): ExerciseStats => {
+  if (args.length < 4) throw new Error("Too few arguments.");
+
+  const exerciseHours: Array<number> = (args.splice(3, ))
+    .map(arg => {
+      if (isNaN(Number(arg))) throw new Error("Enter a valid hours.");
+      return Number(arg);
+    });
+
+  const target: string = args[args.length - 1];
+  if (isNaN(Number(target))) throw new Error("Enter a valid target value.");
+
+  return {
+    exerciseHours,
+    target: Number(target)
+  };
+}
+
+
+const calculateExercises = (
+  exerciseHours: Array<number>,
+  target: number
+): ExerciseResult=> {
+  if (target === 0) throw new Error("target cannot be 0.");
+
+  const periodLength: number = exerciseHours.length;
+  const trainingDays: Array<number> = exerciseHours.filter(hour => hour != 0);
+  const totalHours : number = exerciseHours.reduce((a, b) => a + b, 0);
+  const average: number = totalHours / periodLength;
+  const performanceRatio: number = (average / target) * 100;
+
+  let rating: number
+  let ratingDescription: string;
+  if (performanceRatio < 50) {
+    rating = 1;
+    ratingDescription = "not too bad but could be better.";
+  }
+  else if (performanceRatio >= 50 && performanceRatio <= 99 ) {
+    rating = 2;
+    ratingDescription = "Good";
+  }
+  else if (performanceRatio > 100) {
+    rating = 3;
+    ratingDescription = "Exceptional";
+  }
+
+  return {
+    periodLength,
+    trainingDays: trainingDays.length,
+    success: average >= target,
+    rating,
+    ratingDescription,
+    target,
+    average
+  };
+}
+
+
+try {
+  const {exerciseHours, target} = parseArguments(process.argv);
+  console.log(calculateExercises(exerciseHours, target));
+}
+catch (error: unknown) {
+  let errorMsg: string = "Something went wrong.";
+  if (error instanceof Error) {
+    errorMsg += " Error: " + error.message;
+  }
+  console.log(errorMsg);
+}
+
+
+// Silence block-scoped redeclaration error
+export {};
